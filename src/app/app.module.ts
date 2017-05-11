610604374eb1fcbbcbdb7e1eba0d307cefcf3185
miloskomlenic2719 <etriks.gaming@gmail.com>
2017-05-11 23:58:14 +0200

    Prvi komit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
